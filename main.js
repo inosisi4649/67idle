8f8607f648ff079d95173c218a57600653355f87
inosisi4649 <natamara226@gmail.com>
2020-09-01 02:49:34 +0900

    Alpha 7
    Real bug fix.

--- main.js
+++ main.js
@@ -9,20 +9,26 @@ var gameData = {
 var savegame = JSON.parse(localStorage.getItem("sixtysevenIdlesave"))
 if (savegame !== null) {
     gameData = savegame
-    document.getElementById("pointbank").innerHTML = "You have " + gameData.point + " Points."
-    document.getElementById("perClickUpgrade").innerHTML = "Rankup! (Currently Rank " + gameData.rank + ") Cost: " + gameData.rankcost + " Points"
 }
+document.getElementById("pointbank").innerHTML = "You have " + gameData.point + " Points."
+document.getElementById("perClickUpgrade").innerHTML = "Rankup! (Currently Rank " + gameData.rank + ") Cost: " + gameData.rankcost + " Points"
 
 function makePoint() {
     gameData.point += gameData.pointPerClick
     document.getElementById("pointbank").innerHTML = "You have " + gameData.point + " Points."
 }
 
+function cheat(string) {
+    if (string == "lotpoint") {
+        gameData.point += 1000000000000000000000000000000000
+    }
+}
+
 function buyRank() {
     if (gameData.point >= gameData.rankcost) {
         gameData.point -= gameData.rankcost
         gameData.rank += 1
-        gameData.rankcost = rankcost * (Math.log(rank+1) / Math.log(2.0))
+        gameData.rankcost = Math.floor(gameData.rankcost*1.2)
         document.getElementById("pointbank").innerHTML = "You have " + gameData.point + " Points."
         document.getElementById("perClickUpgrade").innerHTML = "Rankup! (Currently Rank " + gameData.rank + ") Cost: " + gameData.rankcost + " Points"
     }
@@ -35,5 +41,3 @@ var mainGameLoop = window.setInterval(function() {
 var saveGameLoop = window.setInterval(function() {
     localStorage.setItem("sixtysevenIdlesave", JSON.stringify(gameData))
   }, 10000)
-
-var value = Math.log(10.0) / Math.log(2.0);
